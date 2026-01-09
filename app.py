eedaa3d6aaa034bcc4a9cdba47d28ebec018c7cc
joshualukejewson <joshuajewson@icloud.com>
2026-01-09 11:05:18 +1000

    added base templates for html login and dashboard page, implemented base login authentication to login page with flask_sqlalchemy db. not incorportated with main shiny tracking functionality yet. tracking still functional.

--- app.py
+++ app.py
@@ -4,22 +4,50 @@ pokemon search functionality and interactive GUI.
 Made by Joshua Luke.
 """
 
-import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
 import poketrack
 
 app = Flask(__name__)
 stored_pokemon = poketrack.retrieve_pokemon_data()
 
+app.secret_key = "naruto_beats_sasuke_as_adults"
+
+# Configuring an SQL Alchemy DB.
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+# Database Model
+class User(db.Model):
+    # Class Variables
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+    def set_password(self, password):
+        self.passwrod_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.passwrod_hash, password)
+
+
+
+
+
+
+
 """
 Index Route that loads the main page, takes pokemon search input and displays the pokemon to the screen.
-
 @param: None
 @return: flask.render_template() function.
 """
-
 @app.route("/", methods=["GET", "POST"])
 def index():
+
+    if "username" in session:
+        return redirect(url_for('dashboard'))
 
     pokemon = None  # This is what we pass to the template
 
@@ -61,4 +89,6 @@ def index():
     return render_template("index.html", pokemon=pokemon)
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
