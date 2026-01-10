7c4888f0303e276f300df69a2adaf5a7185e6589
joshualukejewson <joshuajewson@icloud.com>
2026-01-11 07:43:07 +1000

    added navbar and moved in template js into script file.

--- static/script.js
+++ static/script.js
@@ -4,3 +4,15 @@ function toggleShiny(img) {
 
   img.src = img.src.includes(shiny) ? normal : shiny;
 }
+
+function setActionForm(action) {
+  let form = document.getElementById("auth-form");
+  if (!form) return;
+  if (action === "login") {
+    form.action = form.dataset.loginUrl;
+  } else if (action === "register") {
+    form.action = form.dataset.registerUrl;
+  }
+  form.method = "post";
+  form.submit();
+}
